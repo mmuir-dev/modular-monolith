f88b0f06f95a30c583c5f236ba9b5d40644f46ea
Maxwell Muir <maxwell.muir@bluetread.com>
2026-06-11 12:58:05 -0700

    Refactored to move items to separate files and also got rid of pre-built boilerplate.

---
+++ RiverBooks.Books/BookService.cs
@@ -0,0 +1,14 @@
+﻿namespace RiverBooks.Books;
+
+internal class BookService : IBookService
+{
+    public IEnumerable<BookDto> ListBooks()
+    {
+        return
+        [
+            new BookDto(Guid.NewGuid(), "The Great Gatsby", "F. Scott Fitzgerald"),
+            new BookDto(Guid.NewGuid(), "To Kill a Mockingbird", "Harper Lee"),
+            new BookDto(Guid.NewGuid(), "1984", "George Orwell")
+        ];
+    }
+}
