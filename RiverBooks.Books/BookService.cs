428abca6f43de067335718f4b9eff4012cf40cd9
Maxwell Muir <maxwell.muir@bluetread.com>
2026-06-12 09:17:03 -0700

    Added editorconfig and books service structure

--- RiverBooks.Books/BookService.cs
+++ RiverBooks.Books/BookService.cs
@@ -1,14 +1,60 @@
-﻿namespace RiverBooks.Books;
+﻿
+namespace RiverBooks.Books;
 
 internal class BookService : IBookService
 {
-    public List<BookDto> ListBooks()
+  private readonly IBookRepository _bookRepository;
+
+  public BookService(IBookRepository bookRepository)
+  {
+    _bookRepository = bookRepository;
+  }
+
+  public async Task CreateBookAsync(BookDto newBook)
+  {
+    var book = new Book(newBook.Id, newBook.Title, newBook.Author, newBook.Price);
+    await _bookRepository.AddAsync(book);
+    await _bookRepository.SaveChangesAsync();
+  }
+
+  public async Task DeleteBookAsync(Guid id)
+  {
+    var bookToDelete = await _bookRepository.GetByIdAsync(id);
+
+    if (bookToDelete is not null)
     {
-        return
-        [
-            new BookDto(Guid.NewGuid(), "The Great Gatsby", "F. Scott Fitzgerald"),
-            new BookDto(Guid.NewGuid(), "To Kill a Mockingbird", "Harper Lee"),
-            new BookDto(Guid.NewGuid(), "1984", "George Orwell")
-        ];
+      await _bookRepository.DeleteAsync(bookToDelete);
+      await _bookRepository.SaveChangesAsync();
     }
+  }
+
+  public async Task<BookDto> GetBookByIdAsync(Guid id)
+  {
+    var book = await _bookRepository.GetByIdAsync(id);
+
+    //TODO: handle not found case
+
+    return new BookDto(book!.Id, book.Title, book.Author, book.Price);
+  }
+
+  public async Task<List<BookDto>> ListBooksAsync()
+  {
+    var books = (await _bookRepository.ListAsync())
+      .Select(book => new BookDto(book.Id, book.Title, book.Author, book.Price))
+      .ToList();
+
+    return books;
+  }
+
+  public async Task UpdateBookPriceAsync(Guid bookId, decimal newPrice)
+  {
+    // validate the price
+
+    var book = await _bookRepository.GetByIdAsync(bookId);
+
+    // hanle not found case
+
+    book!.UpdatePrice(newPrice);
+    await _bookRepository.SaveChangesAsync();
+  }
 }
---
+++ RiverBooks.Books/IReadOnlyBookRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace RiverBooks.Books;
+
+internal interface IReadOnlyBookRepository
+{
+  Task<Book?> GetByIdAsync(Guid id);
+  Task<List<Book>> ListAsync();
+
+}
