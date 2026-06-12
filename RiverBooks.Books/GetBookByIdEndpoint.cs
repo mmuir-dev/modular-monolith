c5db3576219aeaf102bdd1814e9e650fcc89e6aa
Maxwell Muir <maxwell.muir@bluetread.com>
2026-06-12 12:34:18 -0700

    Added Get Books By Id functionality

---
+++ RiverBooks.Books/GetBookByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using FastEndpoints;
+
+namespace RiverBooks.Books;
+
+internal class GetBookByIdEndpoint(IBookService bookService) : Endpoint<GetBookByIdRequest, BookDto>
+{
+  private readonly IBookService _bookService = bookService;
+
+  public override void Configure()
+  {
+    Get("/books/{id}");
+    AllowAnonymous();
+  }
+
+  public override async Task HandleAsync(GetBookByIdRequest req, CancellationToken ct)
+  {
+    var book = await _bookService.GetBookByIdAsync(req.Id);
+
+    if (book is null)
+    {
+      await Send.NotFoundAsync(ct);
+      return;
+    }
+
+    await Send.OkAsync(book, ct);
+  }
+}
